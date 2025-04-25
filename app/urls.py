54b3618c77fbffbefa147b9742dfef01fc52d89d
Nicolas Duarte <nickboytube0418@gmail.com>
2025-04-25 10:56:00 -0300

    ...

--- app/urls.py
+++ app/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('login/', view=LoginView.as_view(), name='login'),
-    path('usuarios/', view=UsuarioListCreateView.as_view(), name='usuario-list-create'),
-    path('usuarios/<int:pk>/', view=UsuarioRetrieveUpdateDestroyView.as_view(), name='usuario-detail'),
-    path('professores/', view=ProfessorListCreateView.as_view(), name='professor-list-create'),
-    path('professores/<int:pk>/', view=ProfessorRetrieveUpdateDestroyView.as_view(), name='professor-detail'),
-    path('disciplinas/', view=DisciplinaListCreateView.as_view(), name='disciplina-list-create'),
-    path('disciplinas/<int:pk>/', view=DisciplinaRetrieveUpdateDestroyView.as_view(), name='disciplina-detail'),
-    path('reservas-ambiente/', view=ReservaAmbienteListCreateView.as_view(), name='reserva-ambiente-list-create'),
+    path('login/', view=LoginView.as_view()),
+    path('usuarios/', view=UsuarioListCreateView.as_view()),
+    path('usuarios/<int:pk>/', view=UsuarioRetrieveUpdateDestroyView.as_view()),
+    path('professores/', view=ProfessorListCreateView.as_view()),
+    path('professores/<int:pk>/', view=ProfessorRetrieveUpdateDestroyView.as_view()),
+    path('disciplinas/', view=DisciplinaListCreateView.as_view()),
+    path('disciplinas/<int:pk>/', view=DisciplinaRetrieveUpdateDestroyView.as_view()),
+    path('reservas-ambiente/', view=ReservaAmbienteListCreateView.as_view())
 ]
